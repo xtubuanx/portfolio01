cd49a2033f08d937f0f871727d30418e5e77a036
xtubuanx <rec.wakabayashi+github@gmail.com>
2018-09-10 05:48:16 +0900

    メインビジュアルのスライダーをslick.jsに変更。

--- shared/js/jquery.settings.js
+++ shared/js/jquery.settings.js
@@ -94,26 +94,75 @@ $(function () {
 
 //////////////////
 /*
+slick.js
+*/
+$('#js-slider-visual').slick({
+    dots: true,
+    arrows: false,
+});
+$('#js-slider-instagram').slick({
+    dots: true,
+    arrows: false,
+    mobileFirst: true,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+        {
+            breakpoint: 1024,
+            /*
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                //unslick: "true"
+            }*/
+            settings: 'unslick'
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+
+
+//////////////////
+/*
 bxslider
 */
-
-$(function () {
-    $('#js-slider-visual').bxSlider({
-        infiniteLoop: true,
-        controls: false
-    });
-});
-$(function () {
-    $('#js-slider-instagram > .p-instagram-grid').bxSlider({
-        infiniteLoop: true,
-        controls: false,
-        responsive: true,
-        minSlides: 5,
-        maxSlides: 5,
-        moveSlides: 5,
-        slideWidth: 100,
-        slideMargin: 10
-        //slideSelector: '#js-slider-visual',
-    });
-
-});
+//
+// jQuery(document).ready(function($){
+//     // $('#js-slider-visual').bxSlider({
+//     //     infiniteLoop: true,
+//     //     controls: false
+//     // });
+//     $('#js-slider-instagram').bxSlider({
+//         useCSS: false,
+//         preventDefaultSwipeY: false,
+//         responsive: true,
+//         minSlides: 5,
+//         maxSlides: 6,
+//         slideWidth: 100,
+//         moveSlides: 6
+//     });
+// });
+// // $(function () {
+// //     $('#js-slider-instagram > .p-instagram-grid').bxSlider({
+// //         infiniteLoop: true,
+// //         controls: false,
+// //         responsive: true,
+// //         minSlides: 5,
+// //         maxSlides: 5,
+// //         moveSlides: 5,
+// //         slideWidth: 100,
+// //         slideMargin: 10
+// //         //slideSelector: '#js-slider-visual',
+// //     });
+// //
+// // });
